126d16fbb0fe5511a2325362b7f98b1d31d4f9ea
Jim Fitzpatrick <jimfity@gmail.com>
2019-06-19 19:30:05 +0100

    Rounting setup completed

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,7 +1,16 @@
 import { library} from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faPhone,
+  faHome,
+  faBirthdayCake,
+  faArrowCircleLeft
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(
   faEnvelope,
-  faPhone
+  faPhone,
+  faHome,
+  faBirthdayCake,
+  faArrowCircleLeft
 );
